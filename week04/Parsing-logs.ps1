e00ee5710faf371b4c2449348c3467c5f95fc699
michael-D-S <michael.sargent@mymail.champlain.edu>
2025-09-23 17:15:20 -0400

    Inital commit for week04

---
+++ week04/Parsing-logs.ps1
@@ -0,0 +1,22 @@
+﻿function ApacheLogs1(){
+$logsNotformatted = Get-Content C:\xampp\apache\logs\access.log
+$tableRecords = @()
+
+for($i=0; $i -lt $logsNotformatted.Count; $i++){
+
+# split a string into words
+$words = $logsNotformatted[$i].Split(" ");
+
+$tableRecords += [PSCustomObject]@{ "IP" = $words[0];
+                    "Time" = $words[3].Trim('[');
+                    "Method" = $words[5].Trim('"');
+                    "Page" = $words[6];
+                    "Protocol" = $words[7].Trim('"');
+                    "Response" = $words[8];
+                    "Referrer" = $words[10];
+                    "Client" = $words[11..($words.Length-1)]; }
+} # end of for loop
+return $tableRecords | Where-Object { $_.IP -like "10.*" }
+}
+$tableRecords = ApacheLogs1
+$tableRecords | Format-Table -AutoSize -Wrap
