b0e94a898e2eb7fe480afe4dc5c6b1a1b9afba62
michael-D-S <michael.sargent@mymail.champlain.edu>
2025-10-28 19:36:37 -0400

    Initial Week 10 Commit

---
+++ week10/ActiveIPList.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# List all the ips in the given network prefix
+# /24 only
+
+# Usage: bash ActiveIPList.bash 10.0.17
+[ $# -ne 1 ] && echo "Usage: $0 <Prefix>" && exit 1
+
+# Prefix is the first input taken
+prefix=$1
+
+# Verify input length
+[ ${#prefix} -lt 5 ] && \
+printf "Prefix length is too short\nPrefix example: 10.0.17\n" && \
+exit 1
+
+for i in {1..254}
+do
+	ping -c 1 $prefix.$i | grep "bytes from" | \
+	grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'
+done
